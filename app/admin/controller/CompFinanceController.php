38016ed183c89c364db80703d9d257e7d79110e2
yangyh <113993051@qq.com>
2017-08-01 16:11:20 +0800

    thinkcmf 代码提交

---
+++ app/admin/controller/CompFinanceController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: YHx
+ * Date: 2017/8/1
+ * Time: 14:42
+ */
+
+namespace app\admin\controller;
+
+
+use cmf\controller\AdminBaseController;
+
+class CompFinanceController extends AdminBaseController
+{
+
+}
---
+++ app/admin/controller/CompOperateController.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: YHx
+ * Date: 2017/8/1
+ * Time: 13:12
+ */
+namespace app\admin\controller;
+
+use cmf\controller\AdminBaseController;
+use think\Db;
+
+/**
+ * Class NavController 导航类别管理控制器
+ * @package app\admin\controller
+ */
+class CompBasicController extends AdminBaseController
+{
+    /**
+     * 导航管理
+     * @adminMenu(
+     *     'name'   => '导航管理',
+     *     'parent' => 'admin/Setting/default',
+     *     'display'=> true,
+     *     'hasView'=> true,
+     *     'order'  => 30,
+     *     'icon'   => '',
+     *     'remark' => '导航管理',
+     *     'param'  => ''
+     * )
+     */
+    public function index()
+    {
+
+        return $this->fetch();
+
+    }
+
+    /**
+     * 添加导航
+     * @adminMenu(
+     *     'name'   => '添加导航',
+     *     'parent' => 'index',
+     *     'display'=> false,
+     *     'hasView'=> true,
+     *     'order'  => 10000,
+     *     'icon'   => '',
+     *     'remark' => '添加导航',
+     *     'param'  => ''
+     * )
+     */
+    public function add()
+    {
+        return $this->fetch();
+    }
+}
