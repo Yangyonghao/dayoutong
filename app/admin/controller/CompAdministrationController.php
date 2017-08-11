049b318f5ba33753690cd27461c8f656130f02fb
yangyh <113993051@qq.com>
2017-08-11 17:32:35 +0800

    行政信息修改

--- app/admin/controller/CompAdministrationController.php
+++ app/admin/controller/CompAdministrationController.php
@@ -10,6 +10,7 @@ namespace app\admin\controller;
 use cmf\controller\AdminBaseController;
 use app\admin\model\CompAdministrationModel;
 use think\Db;
+use think\Exception;
 
 /**
  * Class CompAdministrationController 行政部管理控制器
@@ -45,33 +46,6 @@ class CompAdministrationController extends AdminBaseController
             ->join('spec_comp_basic w','a.comp_id = w.id')->field('a.id as admin_id,a.*,w.comp_name,w.id')
             ->where($where)
             ->order("a.id DESC")->paginate(10);
-//        foreach ($result_list as $v){
-//            $aa[]=$v;
-//        }
-//        var_dump($aa);die;
-
-        $b=['is_website' => "是"
-            ,'evil_network' => "否"
-            ,'criminal_law' => 0
-            ,'civil_law' => 2
-            ,'bank_credit' => "否"
-            ,'abnormal_operation' => "是"
-            ,'illegal_dishonesty' => "否"
-            ,'legal_disputes' => 0];
-
-        $a=[
-            'is_website' => "否",
-            ' evil_network' => "否"
-            ,'criminal_law' => 1
-            ,'civil_law' => "否"
-            ,'bank_credit' => "否"
-            ,'abnormal_operation' => "0"
-            ,'illegal_dishonesty' => "2"
-            ,'legal_disputes' => "0"
-            ,'admin_id' => "2"];
-        $ssp=array_diff_assoc($a,$b);
-        dump($ssp);die;
-
         // 获取分页显示
         $page = $result_list->render();
         $this->assign('result_list',$result_list);
@@ -143,36 +117,166 @@ class CompAdministrationController extends AdminBaseController
      * */
     public function editPost(){
         if ($this->request->isPost()) {
+            $admin_id=$this->request->param('admin_id');
+            $admin_info=Db::name('comp_administration')->field('legal_disputes,civil_law,criminal_law,is_website,evil_network,bank_credit,abnormal_operation,illegal_dishonesty')->where('id',$admin_id)->find();
+
             $CompAdministrationModel = new CompAdministrationModel();
             $post=$this->request->param();
             $result = $this->validate($post, 'CompAdministration');
             if ($result !== true) {
                 $this->error($result);
             }
-            $admin_id=$this->request->param('admin_id');
-            $admin_info=Db::name('comp_administration')->field('is_website,evil_network,criminal_law,civil_law,bank_credit,abnormal_operation,illegal_dishonesty,legal_disputes')->where('id',$admin_id)->find();
-//            $result = $CompAdministrationModel->editCompAdministration($post);
+            $result_info=$CompAdministrationModel->editCompAdministration($post);
+            if($result_info){
+                //取差集
+                $ssp=array_diff_assoc($post,$admin_info);
 
-            $ssp=array_diff_assoc($admin_info,$post);
-            dump($ssp);
-            if ($result === false) {
-                $this->error('添加失败!');
+                $comp_id    =   $ssp["comp_id"];
+                unset($ssp["comp_id"]);unset($ssp["admin_id"]);
+                //获取字段相应的分数数组
+                $result =  $this->getScoreRole($ssp,$admin_info);
+                $i=0;
+                foreach ($result as $key => $value){
+                    $app[$i]['score']=$value["score"];
+                    $app[$i]['score_source']=$value["remark"];
+                    $app[$i]['comp_id']=$comp_id;
+                    $app[$i]['department_type']='金融部数据';
+                    $app[$i]['add_time']=date('Y-m-d H:i:s');
+                    $app[$i]['key_name']=$key;
+                    $app[$i]['ip']=get_client_ip();
+                    Db::name('comp_score_log')->insert($app[$i]);
+                    $i+=1;
+                }
+
+                $data=[
+                    'comp_id'=>$comp_id,
+                    'department_type'=>'行政部数据'
+                ];
+                $data_arr=['comp_id'=>$comp_id];
+                $score=Db::name('comp_score_log')->where($data)->sum('score');
+                $score_detail=Db::name('comp_score')->field('total_score,admin_score')->where($data_arr)->find();
+                $total_score=$score_detail['total_score']-$score_detail['admin_score']+$score;
+                $comp_score=[
+                    'comp_id'=>$comp_id,
+                    'total_score'=>$total_score,
+                    'admin_score'=>$score,
+                ];
+                Db::name('comp_score')->where($data_arr)->update($comp_score);
+
+                $this->success('保存成功!', url('CompAdministration/index'));
+            }else{
+                $this->error('保存失败!');
             }
-
-            $this->success('保存成功!', url('CompAdministration/index'));
         }
     }
-    public function scoreRole($artitude_score_count){
-        $account_score = array(
-            "comp_name"            => array("remark" => "添加名称获取1积分", "score" => "1"),
-            'comp_classify'        => array("remark" => "企业分类获取1分","score" => "1"),
-            'reg_time'             => array("remark" => "填写成立时间记1分","score" => "1"),
-            'reg_money'            => array("remark" => "填写注册资本记1分","score" => "1"),
-            'legal_person'         => array("remark" => "填写企业法人记1分","score" => "1"),
-            'link_addr'            => array("remark" => "填写地址记1分","score" => "1"),
-            'business_license_pic' => array("remark" => "上传营业执照记1分","score" => "1"),
-            'comp_aptitude'        => array("remark" => '添加附加资质记'.$artitude_score_count.'分',"score" => $artitude_score_count),
-        );
+
+
+    public function lostScore($data){
+        //bank_credit //是否被银行列入不诚信名单，是或者否   选择否， 记3分
+        //abnormal_operation //是否被列入经营异常名录      选择否，记2分
+        //illegal_dishonesty //否被列入严重违法失信企业名单 选择否，记2分
+        //legal_disputes //企业民事法律纠纷次数            0记两分
+        //civil_law //股东、法人、高管民事法律纠纷次数       0记两分
+        //criminal_law //股东、法人、高管刑事法律纠纷次数    0记两分
+        //is_website //是否有公司官网 0记两分              选择是， 记1分
+        //evil_network //是否有网络搜索恶评 0记两分         选择是， 记1分
+
+        $account_score =[];
+        //是否被银行列入不诚信名单，是或者否   选择否， 记3分
+        if(isset($data['bank_credit']) && $data['bank_credit']=='是'){
+            $account_score['bank_credit']=["remark" => "选择是记-3分", "score" => "-3"];
+        }elseif(isset($data['abnormal_operation']) && $data['abnormal_operation']=='是'){
+            $account_score['abnormal_operation']=["remark" => "选择是记2分", "score" => "-2"];
+        }elseif(isset($data['illegal_dishonesty']) && $data['illegal_dishonesty']=='是'){
+            //否被列入严重违法失信企业名单 选择否，记2分
+            $account_score['illegal_dishonesty']=["remark" => "选择否记-2分", "score" => "-2"];
+        }elseif(isset($data['legal_disputes']) && $data['legal_disputes']>0){
+            //企业民事法律纠纷次数0次记2分
+            $account_score['legal_disputes']=["remark" => "纠纷次数大于0记-2分", "score" => "-2"];
+        }elseif(isset($data['civil_law']) && $data['civil_law']>0){
+            //股东、法人、高管民事法律纠纷次数
+            $account_score['civil_law']=["remark" => "纠纷次数大于0记-2分", "score" => "-2"];
+        }elseif(isset($data['criminal_law']) && $data['criminal_law']>0){
+            //股东、法人、高管刑事法律纠纷次数选择否，记2分
+            $account_score['criminal_law']=["remark" => "纠纷次数大于0记-2分", "score" => "-2"];
+        }elseif(isset($data['is_website']) && $data['is_website']=='否'){
+            //是否有公司官网选择是， 记1分
+            $account_score['is_website']=["remark" => "选择是记-1分", "score" => "-1"];
+        }elseif(isset($data['evil_network']) && $data['evil_network']=='否'){
+            //是否有网络搜索恶评选择是记1分
+            $account_score['evil_network']=["remark" => "选择是记-1分", "score" => "-1"];
+        }
+        return $account_score;
+    }
+
+
+    //分数加法计算规则
+    public function getScoreRole($data,$old_info){
+        //bank_credit //是否被银行列入不诚信名单，是或者否   选择否， 记3分
+        //abnormal_operation //是否被列入经营异常名录      选择否，记2分
+        //illegal_dishonesty //否被列入严重违法失信企业名单 选择否，记2分
+        //legal_disputes //企业民事法律纠纷次数            0记两分
+        //civil_law //股东、法人、高管民事法律纠纷次数       0记两分
+        //criminal_law //股东、法人、高管刑事法律纠纷次数    0记两分
+        //is_website //是否有公司官网 0记两分              选择是， 记1分
+        //evil_network //是否有网络搜索恶评 0记两分         选择是， 记1分
+        $account_score =[];
+
+        //是否被银行列入不诚信名单，是或者否   选择否， 记3分
+        if(isset($data['bank_credit'])){
+            $bank_credit = $data['bank_credit']=='否' ? ["remark" => "没有被银行列入不诚信名单，加3分", "score" => "+3"] : ["remark" => "被银行列入不诚信名单，减3分", "score" => "-3"];
+            $account_score['bank_credit']  =    $bank_credit;
+        }
+
+        if(isset($data['abnormal_operation'])){
+            //是否被列入经营异常名录
+            $abnormal_operation=$data['abnormal_operation']=='否'?["remark" => "没有被列入经营异常名录，加2分", "score" => "+2"]:["remark" => "被列入经营异常名录，减2分", "score" => "-2"];
+            $account_score['abnormal_operation']=$abnormal_operation;
+        }
+
+        if(isset($data['illegal_dishonesty'])) {
+            //否被列入严重违法失信企业名单 选择否，记2分
+            $illegal_dishonesty=$data['illegal_dishonesty'] == '否'?["remark" => "没有被列入严重违法失信企业名单，加2分", "score" => "+2"]:["remark" => "被列入严重违法失信企业名单，减2分", "score" => "-2"];
+            $account_score['illegal_dishonesty']=$illegal_dishonesty;
+        }
+        if(isset($data['is_website'])) {
+            //是否有公司官网选择是， 记1分
+            $is_website=$data['is_website'] == '是'?["remark" => "有公司官网，加1分", "score" => "+1"]:["remark" => "没有有公司官网，减1分", "score" => "-1"];
+            $account_score['is_website'] =$is_website ;
+        }
+
+        if(isset($data['evil_network'])) {
+            //是否有网络搜索恶评选择是记1分
+            $evil_network=$data['evil_network'] == '否'?["remark" => "没有网络搜索恶评，加1分", "score" => "+1"]:["remark" => "有网络搜索恶评减1分", "score" => "-1"];
+            $account_score['evil_network']=$evil_network;
+        }
+
+        //企业民事法律纠纷次数0次记2分
+        if(isset($data['legal_disputes'])) {
+            if($old_info['legal_disputes']==0 && $data['legal_disputes'] >0 ){
+                $account_score['legal_disputes']=["remark" => "企业民事法律纠纷".$data['legal_disputes']."次，减2分", "score" => "-2"];
+            }elseif($old_info['legal_disputes'] >0 && $data['legal_disputes']==0){
+                $account_score['legal_disputes']=["remark" => "企业民事法律纠纷".$data['legal_disputes']."次，加2分", "score" => "+2"];
+            }
+        }
+
+        //股东、法人、高管民事法律纠纷次数
+        if(isset($data['civil_law'])) {
+            if($old_info['civil_law'] ==0 && $data['civil_law']>0){
+                $account_score['civil_law']=["remark" => "股东、法人、高管民事法律纠纷".$data['civil_law']."次，减2分", "score" => "-2"];
+            }elseif($old_info['civil_law'] >0 && $data['civil_law']==0){
+                $account_score['civil_law']=["remark" => "股东、法人、高管民事法律纠纷".$data['civil_law']."次，加2分", "score" => "+2"];
+            }
+        }
+
+        //股东、法人、高管刑事法律纠纷次数选择否，记2分
+        if(isset($data['criminal_law'])) {
+            if($old_info['criminal_law'] ==0 && $data['criminal_law']>0){
+                $account_score['criminal_law']=["remark" => "股东、法人、高管刑事法律纠纷".$data['criminal_law']."次，减2分", "score" => "-2"];
+            }elseif($old_info['criminal_law'] >0 && $data['criminal_law']==0){
+                $account_score['criminal_law']=["remark" => "股东、法人、高管刑事法律纠纷".$data['criminal_law']."次，加2分", "score" => "+2"];
+            }
+        }
         return $account_score;
     }
 }
--- app/admin/model/CompAdministrationModel.php
+++ app/admin/model/CompAdministrationModel.php
@@ -67,51 +67,51 @@ class CompAdministrationModel extends Model
 
         //是否被银行列入不诚信名单，是或者否   选择否， 记3分
         if($data['bank_credit']=='否'){
-            $account_score['bank_credit']=["remark" => "选择否记3分", "score" => "+3"];
+            $account_score['bank_credit']=["remark" => "没有被银行列入不诚信名单，加3分", "score" => "+3"];
         }else{
             unset($data['bank_credit']);
         }
         //是否被列入经营异常名录
         if($data['abnormal_operation']=='否'){
-            $account_score['abnormal_operation']=["remark" => "选择否记2分", "score" => "+2"];
+            $account_score['abnormal_operation']=["remark" => "没有被列入经营异常名录，加2分", "score" => "+2"];
         }else{
             unset($data['abnormal_operation']);
         }
         //否被列入严重违法失信企业名单 选择否，记2分
         if($data['illegal_dishonesty']=='否'){
-            $account_score['illegal_dishonesty']=["remark" => "选择否记2分", "score" => "+2"];
+            $account_score['illegal_dishonesty']=["remark" => "没有被列入严重违法失信企业名单，加2分", "score" => "+2"];
         }else{
             unset($data['illegal_dishonesty']);
         }
         //企业民事法律纠纷次数0次记2分
         if($data['legal_disputes']==0){
-            $account_score['legal_disputes']=["remark" => "输入0次记2分", "score" => "+2"];
+            $account_score['legal_disputes']=["remark" => "企业民事法律纠纷".$data['legal_disputes']."次,加2分", "score" => "+2"];
         }else{
             unset($data['legal_disputes']);
         }
 
         //股东、法人、高管民事法律纠纷次数
         if($data['civil_law']==0){
-            $account_score['civil_law']=["remark" => "输入0次记2分", "score" => "+2"];
+            $account_score['civil_law']=["remark" => "股东、法人、高管民事法律纠纷".$data['civil_law']."次，加2分", "score" => "+2"];
         }else{
             unset($data['civil_law']);
         }
 
         //股东、法人、高管刑事法律纠纷次数选择否，记2分
         if($data['criminal_law']==0){
-            $account_score['criminal_law']=["remark" => "输入0次记2分", "score" => "+2"];
+            $account_score['criminal_law']=["remark" => "股东、法人、高管刑事法律纠纷".$data['criminal_law']."次，加2分", "score" => "+2"];
         }else{
             unset($data['criminal_law']);
         }
         //是否有公司官网选择是， 记1分
         if($data['is_website']=='是'){
-            $account_score['is_website']=["remark" => "选择是记1分", "score" => "+1"];
+            $account_score['is_website']=["remark" => "有公司官网记1分", "score" => "+1"];
         }else{
             unset($data['is_website']);
         }
         //是否有网络搜索恶评选择是记1分
-        if($data['evil_network']=='是'){
-            $account_score['evil_network']=["remark" => "选择是记1分", "score" => "+1"];
+        if($data['evil_network']=='否'){
+            $account_score['evil_network']=["remark" => "没有网络搜索恶评加1分", "score" => "+1"];
         }else{
             unset($data['evil_network']);
         }
@@ -126,103 +126,14 @@ class CompAdministrationModel extends Model
     public function getCompScoreBasic($id){
         return Db::name('comp_score')->where('comp_id',$id)->find();
     }
-
+    /*
+     * @function:修改行政部管理明细
+     * @date:20170811
+     * */
     public function editCompAdministration($post_param){
-        $admin_id=$post_param['id'];$comp_id=$post_param['comp_id'];unset($post_param['comp_id']);unset($post_param['id']);
-        $i=0;
-        foreach ($post_param as $key=>$val){
-            $result[$i]['score']=Db::name('comp_score_log')->where(['comp_id'=>$comp_id,'key_name'=>$key])->sum('score');
-            $result[$i]['key_name']=$key;
-            $i+=1;
-        }
-        foreach ($result as $k=>$value){
-            if($value>0){
-
-            }else{
-
-            }
-        }
-        dump($result);
-        dump($post_param);
-        die;
+        $admin_id=$post_param['admin_id'];unset($post_param['admin_id']);
         //添加到公司信息表
-//        $result_id=Db::name('comp_administration')->where('id',$admin_id)->update($post_param);
+        $result_id=Db::name('comp_administration')->where('id',$admin_id)->update($post_param);
         return $result_id;
-    }
-
-    function aaa($comp_id,$key_name){
-        $app['score']='+1';
-        $app['score_source']='选择否加1分';
-        $app['comp_id']=$comp_id;
-        $app['department_type']='金融部数据';
-        $app['add_time']=date('Y-m-d H:i:s');
-        $app['key_name']=$key_name;
-        $app['ip']=get_client_ip();
-        $score_status=Db::name('comp_score_log')->insertGetId($app);
-    }
-    public function lostScore($data){
-        //bank_credit //是否被银行列入不诚信名单，是或者否   选择否， 记3分
-        //abnormal_operation //是否被列入经营异常名录      选择否，记2分
-        //illegal_dishonesty //否被列入严重违法失信企业名单 选择否，记2分
-        //legal_disputes //企业民事法律纠纷次数            0记两分
-        //civil_law //股东、法人、高管民事法律纠纷次数       0记两分
-        //criminal_law //股东、法人、高管刑事法律纠纷次数    0记两分
-        //is_website //是否有公司官网 0记两分              选择是， 记1分
-        //evil_network //是否有网络搜索恶评 0记两分         选择是， 记1分
-
-        $account_score =[];
-
-        //是否被银行列入不诚信名单，是或者否   选择否， 记3分
-        if($data['bank_credit']=='是'){
-            $account_score['bank_credit']=["remark" => "选择是记-3分", "score" => "-3"];
-        }else{
-            unset($data['bank_credit']);
-        }
-        //是否被列入经营异常名录
-        if($data['abnormal_operation']=='是'){
-            $account_score['abnormal_operation']=["remark" => "选择是记2分", "score" => "-2"];
-        }else{
-            unset($data['abnormal_operation']);
-        }
-        //否被列入严重违法失信企业名单 选择否，记2分
-        if($data['illegal_dishonesty']=='是'){
-            $account_score['illegal_dishonesty']=["remark" => "选择否记-2分", "score" => "-2"];
-        }else{
-            unset($data['illegal_dishonesty']);
-        }
-        //企业民事法律纠纷次数0次记2分
-        if($data['legal_disputes']>0){
-            $account_score['legal_disputes']=["remark" => "纠纷次数大于0记-2分", "score" => "-2"];
-        }else{
-            unset($data['legal_disputes']);
-        }
-
-        //股东、法人、高管民事法律纠纷次数
-        if($data['civil_law']>0){
-            $account_score['civil_law']=["remark" => "纠纷次数大于0记-2分", "score" => "-2"];
-        }else{
-            unset($data['civil_law']);
-        }
-
-        //股东、法人、高管刑事法律纠纷次数选择否，记2分
-        if($data['criminal_law']>0){
-            $account_score['criminal_law']=["remark" => "纠纷次数大于0记-2分", "score" => "-2"];
-        }else{
-            unset($data['criminal_law']);
-        }
-        //是否有公司官网选择是， 记1分
-        if($data['is_website']=='否'){
-            $account_score['is_website']=["remark" => "选择是记-1分", "score" => "-1"];
-        }else{
-            unset($data['is_website']);
-        }
-        //是否有网络搜索恶评选择是记1分
-        if($data['evil_network']=='否'){
-            $account_score['evil_network']=["remark" => "选择是记-1分", "score" => "-1"];
-        }else{
-            unset($data['evil_network']);
-        }
-
-        return ['a'=>$account_score,'b'=>$data];
     }
 }
