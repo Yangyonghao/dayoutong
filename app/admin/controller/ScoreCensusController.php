13642197c2c49cb7a4501fc6d028ac8977ed0cff
yangyh <113993051@qq.com>
2017-08-18 17:19:35 +0800

    提交

--- app/admin/controller/ScoreCensusController.php
+++ app/admin/controller/ScoreCensusController.php
@@ -33,15 +33,33 @@ class ScoreCensusController extends AdminBaseController
         return $this->fetch();
     }
 
-
+    //总分数记录表
     public function scoreList(){
+        $sql="SELECT scs.total_score,a.*
+              FROM spec_comp_score AS scs
+                LEFT JOIN (
+                    SELECT
+                        SUM(score) AS score,
+                        account_time,
+                        comp_id
+                    FROM
+                        spec_total_score
+                    WHERE
+                        account_time = '2017'
+                    GROUP BY
+                        comp_id
+              ) AS a ON scs.comp_id = a.comp_id";
+        $scoreList=Db::query($sql);
+//        dump($result);die;
+
+
         //分数统计列表
-
-
-        $result_list=Db::name('comp_score')
-            ->alias('a')->field('a.id as score_id,a.*,w.comp_name')
-            ->join('spec_comp_basic w','a.comp_id = w.id')
-            ->where($where)
-            ->order("a.total_score DESC")->paginate(10);
+//        $scoreList=Db::name('comp_score')
+//            ->alias('a')->field('a.id as score_id,a.*,w.comp_name')
+//            ->join('spec_comp_basic w','a.comp_id = w.id')
+//            ->join('spec_comp_basic w','a.comp_id = w.id')
+//            ->order("a.total_score DESC")->paginate(10);
+        $this->assign('scoreList',$scoreList);
+        return $this->fetch();
     }
 }
